6401cebfbed37a1ef95b5cd96ac0d83a008662ff
fjalowiecki <fjalowiecki.42@gmail.com>
2024-12-06 17:52:43 +0100

    shadow fix

--- src/main.c
+++ src/main.c
@@ -5,113 +5,82 @@ float calculate_viewport_width(float focal_length, float fov_degrees) {
     return 2.0 * focal_length * tan(fov_radians / 2.0);
 }
 
-void init_scene(t_data *data) //t_view *view, t_light **light, t_object **obj_arr)
-{
-	t_sphere *sph1 = malloc(sizeof(t_sphere));
-	t_sphere *sph2 = malloc(sizeof(t_sphere));
-	t_plane *plane = malloc(sizeof(t_plane));
-	t_plane *plane2 = malloc(sizeof(t_plane));
-	t_cylinder *cylinder = malloc(sizeof(t_cylinder));
-	t_cone *cone = malloc(sizeof(t_cone));
+void init_sphere(t_sphere *sph, float x, float y, float z, float radius, int r, int g, int b) {
+    sph->center = (t_vec3){x, y, z};
+    sph->radius = radius;
+    sph->color = (t_color){r, g, b};
+}
 
-	data->objects = malloc(sizeof(t_object) * 6);
-	data->objects[0].type = 0;
-	data->objects[0].object = sph1;
-	data->objects[1].type = 0;
-	data->objects[1].object = sph2;
-	data->objects[2].type = 1;
-	data->objects[2].object = plane;
-	data->objects[3].type = 1;
-	data->objects[3].object = plane2;
-	data->objects[4].type = 2;
-	data->objects[4].object = cylinder;
-	data->objects[5].type = 3;
-	data->objects[5].object = cone;
+void init_plane(t_plane *plane, float x, float y, float z, float nx, float ny, float nz, int r, int g, int b) {
+    plane->center = (t_vec3){x, y, z};
+    plane->N = (t_vec3){nx, ny, nz};
+    plane->color = (t_color){r, g, b};
+}
 
-	data->objects_cnt = 6;
+void init_cylinder(t_cylinder *cyl, float x, float y, float z, float diameter, float height, float nx, float ny, float nz, int r, int g, int b) {
+    cyl->center = (t_vec3){x, y, z};
+    cyl->diameter = diameter;
+    cyl->height = height;
+    cyl->N_axis_vec = (t_vec3){nx, ny, nz};
+    cyl->color = (t_color){r, g, b};
+}
 
-	data->view = malloc(sizeof(t_view));
-	data->view->camera_center.x = 0;
-	data->view->camera_center.y = 0;
-	data->view->camera_center.z = 0;
-	data->view->focal_length.x = 0;
-	data->view->focal_length.y = 0;
-	data->view->focal_length.z = 1;
-	data->view->image_width = IMAGE_WIDTH;
-	data->view->image_height = IMAGE_HEIGHT;
-	data->view->viewport_height = 2.0;
-	data->view->fov_degrees = 70;
-	data->view->viewport_width = calculate_viewport_width(data->view->focal_length.z, data->view->fov_degrees);
-	data->view->viewport_height = data->view->viewport_width / (data->view->image_width/data->view->image_height);
+void init_cone(t_cone *cone, float x, float y, float z, float angle, float nx, float ny, float nz, float height, int r, int g, int b) {
+    cone->vertex = (t_vec3){x, y, z};
+    cone->angle = angle;
+    cone->axis = (t_vec3){nx, ny, nz};
+    cone->height = height;
+    cone->color = (t_color){r, g, b};
+}
 
-	data->amb_light = malloc(sizeof(t_light));
-	data->amb_light->origin = (t_vec3){0, 0, 0};
-	data->amb_light->brightness = 0.1;
-	data->amb_light->color = (t_color){255, 255, 255};
+void init_light(t_light *light, float x, float y, float z, float brightness, int r, int g, int b) {
+    light->origin = (t_vec3){x, y, z};
+    light->brightness = brightness;
+    light->color = (t_color){r, g, b};
+}
 
-	data->diff_lights = malloc(sizeof(t_light) * 2);
-	data->diff_lights[0].origin.x = -30;
-	data->diff_lights[0].origin.y = 100;
-	data->diff_lights[0].origin.z = -20;
-	data->diff_lights[0].brightness = 0.5;
-	data->diff_lights[0].color = (t_color){48, 41, 181};
-	data->diff_lights[1].origin.x = -10;
-	data->diff_lights[1].origin.y = 10;
-	data->diff_lights[1].origin.z = 10;
-	data->diff_lights[1].brightness = 0.4;
-	data->diff_lights[1].color = (t_color){120, 37, 37};
-	data->diff_lights_cnt = 2;
+void init_scene(t_data *data) {
+    t_sphere *sph1 = malloc(sizeof(t_sphere));
+    t_sphere *sph2 = malloc(sizeof(t_sphere));
+    t_plane *plane = malloc(sizeof(t_plane));
+    t_cylinder *cylinder = malloc(sizeof(t_cylinder));
+    t_cone *cone = malloc(sizeof(t_cone));
 
-	sph1->center.x = -15;
-	sph1->center.y = 0;
-	sph1->center.z = -100;
-	sph1->radius = 15;
-	sph1->color.r = 148;
-	sph1->color.g = 25;
-	sph1->color.b = 25;
+    data->objects = malloc(sizeof(t_object) * 5);
+    data->objects[0].type = 0;
+    data->objects[0].object = sph1;
+    data->objects[1].type = 0;
+    data->objects[1].object = sph2;
+    data->objects[2].type = 1;
+    data->objects[2].object = plane;
+    data->objects[3].type = 2;
+    data->objects[3].object = cylinder;
+    data->objects[4].type = 3;
+    data->objects[4].object = cone;
 
-	sph2->center.x = 10;
-	sph2->center.y = -5;
-	sph2->center.z = -100;
-	sph2->radius = 10;
-	sph2->color.r = 47;
-	sph2->color.g = 184;
-	sph2->color.b = 37;
+    data->objects_cnt = 5;
 
-	plane->center.x = 0;
-	plane->center.y = -15;
-	plane->center.z = 0;
-	plane->N.x = 0;
-	plane->N.y = 1;
-	plane->N.z = 0;
-	plane->color.r = 26;
-	plane->color.g = 16;
-	plane->color.b = 161;
+    data->view = malloc(sizeof(t_view));
+    data->view->camera_center = (t_vec3){0, 0, 0};
+    data->view->focal_length = (t_vec3){0, 0, 1};
+    data->view->image_width = IMAGE_WIDTH;
+    data->view->image_height = IMAGE_HEIGHT;
+    data->view->fov_degrees = 70;
+    data->view->viewport_width = calculate_viewport_width(data->view->focal_length.z, data->view->fov_degrees);
+    data->view->viewport_height = data->view->viewport_width / (data->view->image_width / data->view->image_height);
 
-	cylinder->center.x = 10;
-	cylinder->center.y = -10;
-	cylinder->center.z = -40;
-	cylinder->diameter = 5;
-	cylinder-> height = 7;
-	cylinder->N_axis_vec.x =  -1;
-	cylinder->N_axis_vec.y =  1;
-	cylinder->N_axis_vec.z =  1;
-	cylinder->color.r = 0;
-	cylinder->color.g = 255;
-	cylinder->color.b = 0;
+    data->amb_light = malloc(sizeof(t_light));
+    init_light(data->amb_light, 0, 0, 0, 0.1, 255, 255, 255);
 
-	cone->angle = 0.7;
-	cone->axis.x = 0;
-	cone->axis.y = -0.1;
-	cone->axis.z = 0;
-	cone->color.r = 255;
-	cone->color.g = 0;
-	cone->color.b = 0;
-	cone->height = 10;
-	cone->vertex.x = 0;
-	cone->vertex.y = 0;
-	cone->vertex.z = -30;
-	
+	data->diff_lights = malloc(sizeof(t_light) * 1);
+	init_light(&data->diff_lights[0], 0, 100, -100, 0.6, 255, 255, 255);
+	data->diff_lights_cnt = 1;
+
+	init_plane(plane, 0, -20, 0, 0, 1, 0, 128, 128, 128);
+	init_sphere(sph1, -50, 0, -100, 20, 255, 0, 0);
+	init_sphere(sph2, -10, 0, -100, 12, 0, 0, 255);
+	init_cylinder(cylinder, 20, -15, -100, 8, 10, 0, 1, 0, 0, 255, 0);
+	init_cone(cone, 50, 0, -100, 0.5, 0, 1, 0, 15, 255, 0, 255);
 }
 
 void	init_window(t_window *window, t_img *img)
--- src/shaded_pixel.c
+++ src/shaded_pixel.c
@@ -1,43 +1,46 @@
 #include "minirt.h"
 
-static bool shadow_ray_hit(t_ray *shadow_ray, t_data *data, int object_index, t_object *objects, float light_distance);
-
-
-bool shaded_pixel(int object_index, t_point3 intersection, t_point3 light, t_data *data)
+static bool	shadow_ray_hit(t_ray *shadow_ray,
+	t_data *data, int object_index, float light_distance)
 {
-	t_ray shadow_ray;
-	float light_distance;
-	
-	shadow_ray.dir = unit_vector(vec_sub(light, intersection));
+	int		i;
+	float	t;
+
+	i = 0;
+	t = -1;
+	while (i < data->objects_cnt)
+	{
+		if (i == object_index || t >= light_distance)
+		{
+			i++;
+			continue ;
+		}
+		if (data->objects[i].type == 0)
+			t = hit_sphere(shadow_ray, (data->objects[i].object));
+		else if (data->objects[i].type == 1)
+			t = hit_plane(shadow_ray, (data->objects[i].object));
+		else if (data->objects[i].type == 2)
+			t = hit_cylinder(shadow_ray, data->objects[i].object);
+		else if (data->objects[i].type == 3)
+			t = hit_cone(shadow_ray, data->objects[i].object);
+		if (t >= 0)
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+bool	shaded_pixel(int object_index,
+	t_point3 intersection, t_point3 light, t_data *data)
+{
+	t_ray	shadow_ray;
+	float	light_distance;
+
 	shadow_ray.orig = intersection;
+	shadow_ray.dir = unit_vector(vec_sub(light, intersection));
 	light_distance = vec_length(vec_sub(light, intersection));
-	if(shadow_ray_hit(&shadow_ray, data, object_index, data->objects, light_distance) == true)
+	if (shadow_ray_hit(&shadow_ray, data, object_index, light_distance) == true)
 		return (true);
 	return (false);
 }
 
-bool shadow_ray_hit(t_ray *shadow_ray, t_data *data, int object_index, t_object *objects, float light_distance)
-{
-	//przechodze przez wszystkie obiekty oprócz tego na ktorym jestem i jezeli t jest wieksze od zero z ktorejkolwiek funckji to zwracam true
-	//chyba ze t jest wieksze niz droga do swiatla
-	int i;
-	float t;
-
-	i = 0;
-	while(i < data->objects_cnt)
-	{
-		if (objects->type == 0)
-			t = hit_sphere(shadow_ray, (objects[i].object));
-		else if (objects->type == 1 && t < light_distance)
-			t = hit_plane(shadow_ray, (objects[i].object));
-		else if (objects->type == 2 && t < light_distance)
-			t = hit_cylinder(shadow_ray, objects[i].object);
-		else if (objects->type == 3 && t < light_distance)
-			t = hit_cone(shadow_ray, objects[i].object);
-		if (t >= 0 && t < light_distance)
-			return(true);
-		i++;
-	}
-
-	return (false);
-}
