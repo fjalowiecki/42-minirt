3a4c04b8ff667bef88dc9be84575e04272d05479
fjalowiecki <fjalowiecki.42@gmail.com>
2024-12-10 16:58:47 +0100

    shadows fix

--- src/shaded_pixel.c
+++ src/shaded_pixel.c
@@ -10,11 +10,6 @@ static bool	shadow_ray_hit(t_ray *shadow_ray,
 	t = -1;
 	while (i < data->objects_cnt)
 	{
-		if (i == object_index || t >= light_distance)
-		{
-			i++;
-			continue ;
-		}
 		if (data->objects[i].type == 0)
 			t = hit_sphere(shadow_ray, (data->objects[i].object));
 		else if (data->objects[i].type == 1)
@@ -23,7 +18,7 @@ static bool	shadow_ray_hit(t_ray *shadow_ray,
 			t = hit_cylinder(shadow_ray, data->objects[i].object);
 		else if (data->objects[i].type == 3)
 			t = hit_cone(shadow_ray, data->objects[i].object);
-		if (t >= 0)
+		if (t > 0.0001 && t < light_distance)
 			return (true);
 		i++;
 	}
@@ -36,6 +31,7 @@ bool	shaded_pixel(int object_index,
 	t_ray	shadow_ray;
 	float	light_distance;
 
+	intersection.z += 0.01;
 	shadow_ray.orig = intersection;
 	shadow_ray.dir = unit_vector(vec_sub(light, intersection));
 	light_distance = vec_length(vec_sub(light, intersection));
